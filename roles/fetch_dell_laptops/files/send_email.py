4590ae3839ac02ccb913826f992f295a0922a282
Ritesh Suresh Raut <155558394+Riteshraut0116@users.noreply.github.com>
2025-07-25 12:09:28 +0530

    Update send_email.py

--- roles/fetch_dell_laptops/files/send_email.py
+++ roles/fetch_dell_laptops/files/send_email.py
@@ -1,12 +1,13 @@
 import smtplib
 import sys
+import os
 from email.mime.multipart import MIMEMultipart
 from email.mime.base import MIMEBase
 from email import encoders
 
 def send_email(email, file_path):
-    from_email = "rautritesh77@gmail.com"
-    from_password = "lgkb mwje iqgh dkqt"  # Use the app password generated from Google
+    from_email = os.getenv("EMAIL_ADDRESS")
+    from_password = os.getenv("EMAIL_PASSWORD")
     to_email = email
 
     msg = MIMEMultipart()
@@ -15,12 +16,12 @@ def send_email(email, file_path):
     msg['Subject'] = "Dell Laptops List"
 
     part = MIMEBase('application', 'octet-stream')
-    part.set_payload(open(file_path, 'rb').read())
+    with open(file_path, 'rb') as file:
+        part.set_payload(file.read())
     encoders.encode_base64(part)
     part.add_header('Content-Disposition', 'attachment; filename="dell_laptops.xlsx"')
     msg.attach(part)
 
-    # Update the SMTP server details here
     mail_server = smtplib.SMTP('smtp.gmail.com', 587)
     mail_server.starttls()
     mail_server.login(from_email, from_password)
